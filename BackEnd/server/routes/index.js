c7ebf44fca049e1b1855fd2b700df3a22a1b6d14
Ncrite1 <145476140+Pigase@users.noreply.github.com>
2025-04-17 02:06:32 +0300

    Add routes
    routes have been added (the ability has been tested) and controllers for them

---
+++ BackEnd/server/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router = new Router()
+const developerRouter = require('./developerRouter')
+const gameRouter = require('./gameRouter')
+const typeRouter = require('./typeRouter')
+const userRouter = require('./userRouter')
+
+router.use('/user',userRouter)
+router.use('/developer',developerRouter)
+router.use('/game',gameRouter)
+router.use('/type',typeRouter)
+  
+module.exports = router
