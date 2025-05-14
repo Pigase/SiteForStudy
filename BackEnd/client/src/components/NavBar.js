f79488949aaef407ac61371ef400776957ee4748
Ncrite1 <145476140+Pigase@users.noreply.github.com>
2025-05-14 03:04:09 +0300

    Pages registration and authorization

--- BackEnd/client/src/components/NavBar.js
+++ BackEnd/client/src/components/NavBar.js
@@ -6,20 +6,27 @@ import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
 import { SHOP_ROUTE } from '../utils/consts';
 import { Button } from 'react-bootstrap';
+import { observer } from 'mobx-react-lite';
 
-const NavBar = () => { 
+const NavBar = observer(() => { 
     const {user} = useContext(Context)
   return (
     <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <NavLink style={{color:'white'}} to = {SHOP_ROUTE}>КупиИгру</NavLink>
+          {user.isAuth ?
           <Nav className="ml-auto" style={{color:'white'}}>
-            <Button variant={"outline-light"}>Админ Панель</Button>
-            <Button variant={"outline-light"}>Авторизация</Button>
+            <Button variant={"outline-light"} className="me-3">Админ Панель</Button>
+            <Button variant={"outline-light"}>Выйти</Button>
           </Nav>
+          :
+          <Nav className="ml-auto" style={{color:'white'}}>
+            <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+          </Nav>
+        }
         </Container>
       </Navbar>
   );
-}
+})
 
 export default NavBar;
