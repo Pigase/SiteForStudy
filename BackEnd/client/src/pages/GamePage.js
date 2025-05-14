1060d6e14da801041b88410236c409c8fb21b6c9
Ncrite1 <145476140+Pigase@users.noreply.github.com>
2025-05-15 02:41:58 +0300

    page GameItem

--- BackEnd/client/src/pages/GamePage.js
+++ BackEnd/client/src/pages/GamePage.js
@@ -1,11 +1,79 @@
 import React from 'react';
+import { Image, Col, Container, Row, Button, Card } from 'react-bootstrap';
+import star from '../assets/StarForRatingShop.png';
 
-const GamePage = () => { 
+const GamePage = () => {
+  const game = {
+    id: 1, 
+    name: "FlappyBird", 
+    price: 5, 
+    rating: 3, 
+    img: "https://clipartflare.com/images/fl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png"
+  };
+  const description = [
+    {id:1, title: 'Требуемая оперативная память', description: '16 гб'},
+    {id:2, title: 'Требуемая Процессор', description: 'Ryzen 7 7700'},
+    {id:3, title: 'Требуемая Видеокарта', description: 'Nvidia 5070'},
+    {id:4, title: 'Требуемая Память', description: '400 мб'},
+    {id:5, title: 'Требуемая разрешение', description: '1940x1080'},
+  ]
+
   return (
-    <div >
-      GamePage
-    </div>
+    <Container className="mt-4">
+      <Row>
+        <Col md={4}>
+          <Image 
+            width={300} 
+            height={300} 
+            src={game.img} 
+            alt={game.name}
+          />
+        </Col>
+        
+        <Col md={4} className="d-flex flex-column align-items-center">
+          <h2 className="text-center">{game.name}</h2>
+          <div 
+            className="d-flex align-items-center justify-content-center"
+            style={{
+              background: `url(${star}) no-repeat center center`,
+              width: 240, 
+              height: 240,
+              backgroundSize: 'cover',
+              position: 'relative'
+            }}
+          >
+            <div style={{
+              position: 'absolute',
+              fontSize: 64,
+              color: 'white',
+              left: '50%',
+              transform: 'translateX(-50%)'
+            }}>
+              {game.rating}
+            </div>
+          </div>
+        </Col>
+        
+        <Col md={4}>
+           <Card
+             className="d-flex flex-column align-items-center justify-content-around"
+             style={{width:300, height:300, fontSize:32, border: '5px solid lightgray'}}
+           >
+              <h3>{game.price} $</h3>
+              <Button variant={"outline-dark"}>Добавить в карзину</Button>
+           </Card>
+        </Col>
+      </Row>
+      <Row className = "d-flex flex-column m-3">
+        <h1>Минимальные характеристики</h1>
+        {description.map((info, index) =>
+        <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+          {info.title}: {info.description}
+        </Row>
+        )}
+      </Row>
+    </Container>
   );
-}
+};
 
 export default GamePage;
--- BackEnd/client/src/store/GameStore.js
+++ BackEnd/client/src/store/GameStore.js
@@ -20,11 +20,11 @@ export default class GameStore
         ]
         this._games =
         [
-            {id: 1, name: "FlappyBird", price: 5, rating: 3, img: "https://yandex.by/images/search?from=tabbar&img_url=https%3A%2F%2Fclipartflare.com%2Fimages%2Ffl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png&lr=153&pos=6&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0%20%D0%B8%D0%B3%D1%80%D1%8B%20300%20%D0%BD%D0%B0%20300%20%D0%B0%D0%B4%D1%84%D0%B7%D0%B7%D0%BD"},
-            {id: 2, name: "FlappyBird", price: 5, rating: 3, img: "https://yandex.by/images/search?from=tabbar&img_url=https%3A%2F%2Fclipartflare.com%2Fimages%2Ffl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png&lr=153&pos=6&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0%20%D0%B8%D0%B3%D1%80%D1%8B%20300%20%D0%BD%D0%B0%20300%20%D0%B0%D0%B4%D1%84%D0%B7%D0%B7%D0%BD"},
-            {id: 3, name: "FlappyBird", price: 5, rating: 3, img: "https://yandex.by/images/search?from=tabbar&img_url=https%3A%2F%2Fclipartflare.com%2Fimages%2Ffl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png&lr=153&pos=6&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0%20%D0%B8%D0%B3%D1%80%D1%8B%20300%20%D0%BD%D0%B0%20300%20%D0%B0%D0%B4%D1%84%D0%B7%D0%B7%D0%BD"},
-            {id: 4, name: "FlappyBird", price: 5, rating: 3, img: "https://yandex.by/images/search?from=tabbar&img_url=https%3A%2F%2Fclipartflare.com%2Fimages%2Ffl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png&lr=153&pos=6&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0%20%D0%B8%D0%B3%D1%80%D1%8B%20300%20%D0%BD%D0%B0%20300%20%D0%B0%D0%B4%D1%84%D0%B7%D0%B7%D0%BD"},
-            {id: 5, name: "FlappyBird", price: 5, rating: 3, img: "https://yandex.by/images/search?from=tabbar&img_url=https%3A%2F%2Fclipartflare.com%2Fimages%2Ffl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png&lr=153&pos=6&rpt=simage&text=%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0%20%D0%B8%D0%B3%D1%80%D1%8B%20300%20%D0%BD%D0%B0%20300%20%D0%B0%D0%B4%D1%84%D0%B7%D0%B7%D0%BD"}
+            {id: 1, name: "FlappyBird", price: 5, rating: 3, img: "https://clipartflare.com/images/fl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png"},
+            {id: 2, name: "FlappyBird", price: 5, rating: 3, img: "https://clipartflare.com/images/fl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png"},
+            {id: 3, name: "FlappyBird", price: 5, rating: 3, img: "https://clipartflare.com/images/fl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png"},
+            {id: 4, name: "FlappyBird", price: 5, rating: 3, img: "https://clipartflare.com/images/fl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png"},
+            {id: 5, name: "FlappyBird", price: 5, rating: 3, img: "https://clipartflare.com/images/fl5889fdb4-flappy-bird-flappy-bird-sprite-png-clipart-full-size-clipart-3703571.png"}
         ]   
         this._selectedType = {}
         this._selectedDeveloper = {}
